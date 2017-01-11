b4b1ab29c054ff2aca5debba90fb8a29987ffcb5
Marvin Medeiros <marvin.medeiros52@gmail.com>
2017-01-11 00:58:21 -0200

    refactor: Eject out react-scritps and generate new build for bower package

--- generate_dist.sh
+++ generate_dist.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-rm -rf dist/css
-rm -rf dist/js
-cp -Rfp build/static/css dist/css
-cp -Rfp build/static/js dist/js
-mv dist/js/main.*.js dist/js/feedcast-player.min.js
-mv dist/css/main.*.css dist/css/feedcast-player.min.css
+# Copy css files
+if [ -f build/static/css/feedcast-player.css ]
+then
+	cp -Rfp build/static/css/feedcast-player.css dist/css/feedcast-player.css
+	cp -Rfp build/static/css/feedcast-player.css.map dist/css/feedcast-player.css.map
+fi
+
+if [ -f build/static/css/feedcast-player.min.css ]
+then
+	cp -Rfp build/static/css/feedcast-player.min.css dist/css/feedcast-player.min.css
+	cp -Rfp build/static/css/feedcast-player.min.css.map dist/css/feedcast-player.min.css.map
+fi
+
+# Copy js files
+if [ -f build/static/js/feedcast-player.js ]
+then
+	cp -Rfp build/static/js/feedcast-player.js dist/js/feedcast-player.js
+	cp -Rfp build/static/js/feedcast-player.js.map dist/js/feedcast-player.js.map
+fi
+
+if [ -f build/static/js/feedcast-player.min.js ]
+then
+	cp -Rfp build/static/js/feedcast-player.min.js dist/js/feedcast-player.min.js
+	cp -Rfp build/static/js/feedcast-player.min.js.map dist/js/feedcast-player.min.js.map
+fi
