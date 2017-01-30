b3dd6b719e34570d44bfde59723d2b85ef4db271
msmedeiros <marvin.medeiros52@gmail.com>
2017-01-30 21:09:22 -0200

    feature: Start player before event onload from window

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ import App from './App';
 
 import './helpers/feedcastPlayer';
 
-window.onload = function(){
-	const fcPlayer = document.querySelector('.feedcast-player')
-
-    if(fcPlayer){
-    	ReactDOM.render(
-    		<App
-    			media-url={fcPlayer.getAttribute('data-media-url') || ''}
-    			download-url={fcPlayer.getAttribute('data-download-url') || ''}
-                next-episode={fcPlayer.getAttribute('data-next-episode') || ''}
-                image-url={fcPlayer.getAttribute('data-image-url') || ''}
-                title={fcPlayer.getAttribute('data-title') || ''}
-    			audio-wave-color={fcPlayer.getAttribute('data-audio-wave-color') || ''}
-                audio-wave={JSON.parse(fcPlayer.getAttribute('data-audio-wave')) || null}
-    		/>,
-    		fcPlayer
-    	)
-    }
-
+function findPlayer(){
+  const fcPlayer = document.querySelector('.feedcast-player')
+  if(fcPlayer){
+    clearInterval(window.findPlayerInterval);
+    ReactDOM.render(
+      <App
+        media-url={fcPlayer.getAttribute('data-media-url') || ''}
+        download-url={fcPlayer.getAttribute('data-download-url') || ''}
+        next-episode={fcPlayer.getAttribute('data-next-episode') || ''}
+        image-url={fcPlayer.getAttribute('data-image-url') || ''}
+        title={fcPlayer.getAttribute('data-title') || ''}
+        audio-wave-color={fcPlayer.getAttribute('data-audio-wave-color') || ''}
+        audio-wave={JSON.parse(fcPlayer.getAttribute('data-audio-wave')) || null}
+      />,
+      fcPlayer
+    )
+  }
 }
+window.findPlayerInterval = setInterval(findPlayer, 5);
